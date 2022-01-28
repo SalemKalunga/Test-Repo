c66a32dcd713377b0afe481ee909d05a0735e00e
Salem Koni <salemlegrand65@gmail.com>
2022-01-28 20:21:45 +0100

    created some buttons in the .ts file and the .js file

---
+++ about.ts
@@ -0,0 +1,5 @@
+//This is a typescript file
+const btn1 = document.createElement("button");
+btn1.addEventListener("click",function(){
+    console.log("this is an event");
+})
---
+++ file1.js
@@ -0,0 +1,5 @@
+//This is a javascript file for some testing purpuse on git
+const btn = document.createElement("button");
+btn.addEventListener('click', function() {
+    console.log("this is for some checking purpose");
+});
