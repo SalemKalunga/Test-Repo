352f95df3ce36243b82ff2c62acc0e8a0586219e
Salem Koni <salemlegrand65@gmail.com>
2022-01-28 20:33:46 +0100

    I created some new html files and modified the file1.js by ading a user

--- file1.js
+++ file1.js
@@ -3,3 +3,9 @@ const btn = document.createElement("button");
 btn.addEventListener('click', function() {
     console.log("this is for some checking purpose");
 });
+
+const name = "Salem Koni";
+const age = 20;
+const gender = "M";
+const country = "DRC";
+const town = "Lubumbashi";
