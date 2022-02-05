420b3324d66545f3ca45d44c3d4ddd5ae3746e99
Salem Koni <salemlegrand65@gmail.com>
2022-02-05 15:27:58 +0100

    added the azuma team in typescript file

--- about.ts
+++ about.ts
@@ -8,3 +8,7 @@ const ninja2 = { name: "Naruto", clan : "Uzumaki" }
 const ninja1 = {name :"Sasuke", clan: "Uchiwa"}
 const ninja3 = {name:"Sakura", clan:"Aruno"}
 const ninja4 = {name:"kakashi", clan:"Atake"}
+const ninja5 = {name:"Azuma", clan:"Sarutobi"}
+const ninja6 = {name:"ino", clan:"yamanaka"}
+const ninja7 = {name:"shikamaru", clan:"naara"}
+const ninja8 = {name:"Shoji", clan:"Akimishi"}
